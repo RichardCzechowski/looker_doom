42f41611c934b5a2a90ca047c6e8f0e5c0eb53b4
Richard Czechowski <richardczechowski@gmail.com>
2018-10-08 14:51:28 -0700

    update to use new doom location

--- doom.js
+++ doom.js
@@ -379,7 +379,7 @@ looker.plugins.visualizations.add({
  `
  var dosbox = new Dosbox({
         id: "dosbox",
-        onload: function (dosbox) {   dosbox.run("https://cdn.rawgit.com/darrengruber/docker-em-dosbox-doom/afad47b2/doom_shareware/doom19s_deiced.zip", "DOOM.EXE");
+        onload: function (dosbox) {   dosbox.run("https://github.com/RichardCzechowski/looker_doom/raw/master/doom19s_deiced.zip", "DOOM.EXE");
         },
         onrun: function (dosbox, app) {
           console.log("App '" + app + "' is running");
